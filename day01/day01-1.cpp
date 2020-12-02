50f90c115f6472655d565b66f4e35a146450484e
Pat Ausel <TheSheepGuy1@gmail.com>
2020-12-02 01:10:47 +0000

    Finish day 1 challenge

---
+++ day01/day01-1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<int> get_numbers_from_file(std::string path) {
+	std::vector<int> vectorToReturn;
+	std::ifstream filestream(path);
+	std::string currentNumberString;
+	while (std::getline(filestream, currentNumberString)) {
+		vectorToReturn.push_back(std::stoi(currentNumberString));
+	}
+	return vectorToReturn;
+}
+
+int get_summing_number(std::vector<int> numbersToCheck, int posCheckAgainst) {
+	int checkAgainst = numbersToCheck[posCheckAgainst];
+	for (int i = posCheckAgainst + 1; i < numbersToCheck.size(); i++) {
+		if (checkAgainst + numbersToCheck[i] == 2020) {
+			return numbersToCheck[i];
+		}
+	}
+	return -1;
+}
+
+int main() {
+	std::string filename = "input.txt";
+	std::vector<int> numbers = get_numbers_from_file(filename);
+	int solution[2] = {0, 0};
+	int index;
+
+	for (int i = 0; i < numbers.size(); i++) {
+		int result = get_summing_number(numbers, i);
+		if (result != -1) {
+			solution[0] = numbers[i];
+			solution[1] = result;
+			index = i;
+		}
+	}
+
+	int answer = solution[0] * solution[1];
+
+	std::cout << std::to_string(answer) << std::endl;
+
+	return 0;
+}
---
+++ day01/day01-2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<int> get_numbers_from_file(std::string path) {
+	std::vector<int> vectorToReturn;
+	std::ifstream filestream(path);
+	std::string currentNumberString;
+	while (std::getline(filestream, currentNumberString)) {
+		vectorToReturn.push_back(std::stoi(currentNumberString));
+	}
+	return vectorToReturn;
+}
+
+int get_summing_number(std::vector<int> numbersToCheck, int posCheckAgainst) {
+	int checkAgainst = numbersToCheck[posCheckAgainst];
+	for (int i = posCheckAgainst + 1; i < numbersToCheck.size(); i++) {
+		if (checkAgainst + numbersToCheck[i] == 2020) {
+			return numbersToCheck[i];
+		}
+	}
+	return -1;
+}
+
+std::vector<int> get_sums(std::vector<int> numbersToCheck, int posToCheck, int depth, std::vector<int> previousIndicies = {}) {
+	std::vector<int> toReturn;
+	if (depth == 1) {
+		int checkAgainst = 0;
+		for (int i = 0; i < previousIndicies.size(); i++) {
+			checkAgainst += numbersToCheck[previousIndicies[i]];
+			toReturn.push_back(numbersToCheck[previousIndicies[i]]);
+		}
+		for (int j = posToCheck + 1; j < numbersToCheck.size(); j++) {
+			if (checkAgainst + numbersToCheck[j] == 2020) {
+				toReturn.push_back(numbersToCheck[j]);
+				return toReturn;
+			}
+		}
+		toReturn.clear();
+	}
+	else {
+		for (int i = posToCheck; i < numbersToCheck.size(); i++) {
+			previousIndicies.push_back(i);
+			std::vector<int> result = get_sums(numbersToCheck, i + 1, depth - 1, previousIndicies);
+			if (result.size() != 0) {
+				return result;
+			}
+			previousIndicies.pop_back();
+		}
+	}
+	return toReturn;
+}
+
+int main() {
+	std::string filename = "input.txt";
+	std::vector<int> numbers = get_numbers_from_file(filename);
+	std::vector<int> solution = get_sums(numbers, 0, 3);
+	int answer = solution[0];
+	for (int i = 1; i < solution.size(); i++) {
+		answer *= solution[i];
+	}
+
+	std::cout << std::to_string(answer) << std::endl;
+
+	return 0;
+}
